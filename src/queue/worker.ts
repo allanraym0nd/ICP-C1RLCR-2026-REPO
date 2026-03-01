d33da8aa65fb989939349ddbd1b529c1d7d534cb
allan raymond <raymonda916@gmail.com>
2026-03-01 19:30:37 +0300

    linked worker to channels

--- src/queue/worker.ts
+++ src/queue/worker.ts
@@ -1,14 +1,49 @@
-import { Worker } from "bullmq";
+import { Worker, Job } from "bullmq";
 import { config } from '../config/index.js'
+import { sendMail } from "../channels/email.js";
+import { sendSMS } from "../channels/sms.js";
+import { sendPush } from "../channels/push.js";
+import { renderTemplate } from "../templates/engine.js";
+import { logNotification } from "../db/logger.js";
+import { type EmailJob, type SMSJob, type PushJob } from '../types/index.js';
+import { type NotificationJob } from "../types/index.js";
 
 const connection = {
     host: config.redis.host,
     port: config.redis.port
 }
 
-const worker = new Worker('notifications',
-    async (job) => {
-        console.log(`processing job [${job.id}] on channel: ${job.data.channel}`)
+const worker = new Worker<NotificationJob>('notifications',
+    async (job: Job<NotificationJob>) => {
+        const { channel } = job.data
+        let status = 'sent'
+
+        try {
+            if (channel === 'email') {
+                const { to, subject, template, data } = job.data as EmailJob;
+                const html = renderTemplate('email', template, data)
+                await sendMail(to, subject, html)
+            }
+            else if (channel === 'sms') {
+                const { to, template, data } = job.data as SMSJob
+                const body = renderTemplate('sms', template, data)
+                await sendSMS(to, body)
+            }
+            else if (channel === 'push') {
+                const { token, title, body } = job.data as PushJob;
+                await sendPush(token, title, body);
+
+            }
+
+            console.log(`processing job [${job.id}] on channel: ${job.data.channel}`)
+        }
+        catch (err) {
+            status = "failed"
+            throw err;
+
+        } finally {
+            await logNotification(channel, job.data, status)
+        }
     },
     { connection, concurrency: 5 }
 )
